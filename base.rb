509c9e3d9ca319a22edcf3adc8bc49db297d2dc0
Benjamin Unger <benjamin.d.unger@gmail.com>
2012-08-16 20:01:22 -0400

    Add base gems

--- base.rb
+++ base.rb
@@ -1,17 +1,24 @@
-gem("pg", "XXX")
-gem("thin", "XXX")
-gem "will_paginate", "~> 3.0.3"
+# it is probably not necessary to add the pg gem if
+# the appropriate postgreSQL flag is used with the 
+# rails new generator
+# gem("pg", "~> 0.14.0")
+gem("thin", "~> 1.4.1")
+gem("will_paginate", "~> 3.0.3")
+gem("simple_form", "~> 2.0.2")
 
 gem_group :development do
- gem "annotate", "~> 2.5.0"
- gem "pry", "~> 0.9.10"
+ gem("annotate", "~> 2.5.0")
+ gem("pry", "~> 0.9.10")
 end
 
 gem_group :test, :development do
- gem "faker", "~> 1.0.1"
+ gem("faker", "~> 1.0.1")
+ gem('rspec-rails', '~> 2.11.0')
 end
 
 gem_group :test do
- gem "spork", "~> 0.9.2"
+ gem("spork", "~> 0.9.2")
+ gem('capybara', '~> 1.1.2')
+ gem('factory_girl_rails', '~> 4.0.0')
 end
 
