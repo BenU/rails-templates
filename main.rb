353949f6ebf6aeab3f47eb6991f9582d38501fab
Benjamin Unger <benjamin.d.unger@gmail.com>
2012-08-30 19:39:21 -0400

    Begin to refactored main.rb with code groups in recipes

---
+++ main.rb
@@ -0,0 +1,18 @@
+# main rails template with all component parts
+
+# add my default gemfiles
+apply "https://raw.github.com/BenU/rails-templates/master/recipes/gemfile.rb"
+
+# create new README.markdown
+apply "https://raw.github.com/BenU/rails-templates/master/recipes/readme.rb"
+
+# start creating individual files again here...
+
+# initiate git, update .gitignore and first commit
+apply "https://raw.github.com/BenU/rails-templates/master/recipes/git.rb"
+
+# update default css and view layout files and partials
+apply "https://raw.github.com/BenU/rails-templates/master/recipes/views.rb"
+
+# ...
+
---
+++ recipes/gemfile.rb
@@ -0,0 +1,21 @@
+# add my default gemfiles
+gem("thin", "~> 1.4.1")
+gem("will_paginate", "~> 3.0.3")
+gem("formtastic", "~> 2.2.1")
+
+gem_group :development do
+ gem("annotate", "~> 2.5.0")
+ gem("pry", "~> 0.9.10")
+end
+
+gem_group :test, :development do
+ gem("faker", "~> 1.0.1")
+ gem('rspec-rails', '~> 2.11.0')
+end
+
+gem_group :test do
+ gem("spork", "~> 0.9.2")
+ gem('capybara', '~> 1.1.2')
+ gem('factory_girl_rails', '~> 4.0.0')
+end
+run 'bundle install'
