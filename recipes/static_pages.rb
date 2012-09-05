6eff17a0bdea8cc5a1ca3768249a016e400b4321
Benjamin Unger <benjamin.d.unger@gmail.com>
2012-09-04 20:58:40 -0400

    Update static pages routes

--- recipes/static_pages.rb
+++ recipes/static_pages.rb
@@ -3,9 +3,27 @@ if yes?("Would you like to generate static pages?")
   static_pages = "home "
   static_pages += ask("In addition to home, what other static pages do you want created?
   Please separate your pages by spaces:")
+  static_pages_array = static_pages.split()
   generate "controller", "StaticPages #{static_pages}"
-  route("root :to => 'static_pages#home'")
+  
+  # root to home page
+  gsub_file('config/routes.rb', /get "static_pages\/home"/, 
+    "root :to => 'static_pages#home'" )
   remove_file "public/index.html"
+  
+  # create named routes for our static pages
+  # *****
+  static_pages_array.each do |static_page|
+    regex_string = "get \"static_pages\\/#{static_page}\""
+    if static_page != "home" 
+      gsub_file('config/routes.rb', Regexp.new(regex_string), 
+        "match '/#{static_page}',  to: 'static_pages\##{static_page}"
+    else
+      gsub_file('config/routes.rb', Regexp.new(regex_string), 
+        ""
+    end   
+  end 
+
 
   git :add => "."
   git :commit => "-am 'Create static pages'"
