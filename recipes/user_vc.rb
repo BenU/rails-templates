b4c12df9effe8d6eeea42ba38392e429f7905cbd
Benjamin Unger <benjamin.d.unger@gmail.com>
2012-09-08 14:33:07 -0400

    add code to offer signup link to home page if adding authentication

--- recipes/user_vc.rb
+++ recipes/user_vc.rb
@@ -16,3 +16,13 @@
   insert_into_file "config/routes.rb", 
   "  match '/signup',  to: 'users#new'\n\n", 
   before: "  # The priority is based upon order of creation:"
+
+  # offer adding 'signup' link to home page if home page exists
+  if File.exists?("app/views/static_pages/home.html.erb")
+    if yes?("Add 'signup' link to home page?")
+      insert_into_file "app/views/static_pages/home.html.erb",
+        "  <div class=\"class_place_holder\">
+            <%= link_to \"Sign up.\", new_user_path, class: \"sign_up_button_placeholder\" %>
+          </div>\n", before: "</p>"
+    end
+  end
