ad134eb4d22b104f56b46ccb44d22653334f96ea
Benjamin Unger <benjamin.d.unger@gmail.com>
2012-09-09 16:06:31 -0400

    correct typo in new code

--- recipes/static_pages.rb
+++ recipes/static_pages.rb
@@ -26,7 +26,7 @@ if yes?("Would you like to generate static pages?")
   # add links to static pages in page footer
   static_pages_array.each do |static_page|
     insert_into_file "app/views/layouts/_footer.html.erb",
-    "\t\t\t<li><%= link_to \"#{static_page}\", #{static_page}_path %></li>\n"
+    "\t\t\t<li><%= link_to \"#{static_page}\", #{static_page}_path %></li>\n",
     before: "</ul>"
   end
 
