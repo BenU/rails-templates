bf89468070be8339a2116b584f2bd13636cf5673
Benjamin Unger <benjamin.d.unger@gmail.com>
2012-09-15 21:09:15 -0400

    strip color methods from new rvm code

--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -3,16 +3,16 @@
 
 # Set up rvm private gemset
 
-puts "Setting up RVM gemset and installing bundled gems (may take a while) ... ".magenta
+puts "Setting up RVM gemset and installing bundled gems (may take a while) ... "
 
 # Need to strip colors in case rvm_pretty_print_flag is enabled in user's .rvmrc
 rvm_list = `rvm list`.gsub(Regexp.new("\e\\[.?.?.?m"), '')
 
 current_ruby = rvm_list.match(/=> ([^ ]+)/)[1]
-desired_ruby = ask("Which RVM Ruby would you like to use? [#{current_ruby}]".red)
+desired_ruby = ask("Which RVM Ruby would you like to use? [#{current_ruby}]")
 desired_ruby = current_ruby if desired_ruby.blank?
 
-gemset_name = ask("What name should the custom gemset have? [#{@app_name}]".red)
+gemset_name = ask("What name should the custom gemset have? [#{@app_name}]")
 gemset_name = @app_name if gemset_name.blank?
 
 # Create the gemset
@@ -23,7 +23,7 @@ run "rvm #{desired_ruby} gemset create #{gemset_name}"
 
 # Create .rvmrc
 file '.rvmrc', @rvm
-puts "                  #{@rvm}".yellow
+puts "                  #{@rvm}"
 
 # Make the .rvmrc trusted
 run "rvm rvmrc trust #{@app_path}"
