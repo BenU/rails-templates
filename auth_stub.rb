e993a0e27922dbaeeaa50f9fe240ce2c746e0360
Benjamin Unger <benjamin.d.unger@gmail.com>
2012-09-21 18:03:46 -0400

    Add code to recipes/authentication

--- auth_stub.rb
+++ auth_stub.rb
@@ -1,2 +1,16 @@
+# main rails template with all component parts
+
+# add my default gemfiles
+apply "~/programing/rails/rails-templates/recipes/gemfile.rb"
+
+# initiate git, update .gitignore and first commit
+apply "~/programing/rails/rails-templates/recipes/git.rb"
+
+# set up database for postgreSQL
+apply "~/programing/rails/rails-templates/recipes/postgresql.rb"
+
+# add rspec, spork and capybara
+apply "~/programing/rails/rails-templates/recipes/rspec.rb"
+
 # authentication
 apply "~/programing/rails/rails-templates/recipes/authentication.rb"
